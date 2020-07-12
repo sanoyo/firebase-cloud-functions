80c80a5dd1b819f22803d2e93d4e62514d7e73c7
sanoyo <sa00380304yo@gmail.com>
2020-07-12 16:59:30 +0900

    fireatoreへの保存実装

--- functions/index.js
+++ functions/index.js
@@ -1,27 +1,43 @@
-const functions = require('firebase-functions');
-const express = require('express');
-const request = require('request')
-// const app = express();
-// const port = 5000
+// Railsへのcallback
+// const functions = require('firebase-functions');
+// const express = require('express');
+// const request = require('request')
 
-exports.callback = functions
-  .region('asia-northeast1')
-  .https.onRequest((req, res) => {
-    console.log(req)
-    console.log('Receive http request')
+// exports.callback = functions
+//   .region('asia-northeast1')
+//   .https.onRequest((req, res) => {
+//     console.log('Receive http request')
 
-    const options = {
-      url: 'https://bccd9eb6fd98.ngrok.io/callback',
-      port: '3000',
-      method: 'POST',
-      headers: {
-        "content-type": "application/json"
-      },
-      body: req,
-      json: true,
-    };
-    console.log(options.body.body)
-    request(options, (error, body) => {
-      res.send(body.body.body);
-    })
-})
+//     const options = {
+//       url: 'https://bccd9eb6fd98.ngrok.io/callback',
+//       port: '3000',
+//       method: 'POST',
+//       headers: {
+//         "content-type": "application/json"
+//       },
+//       body: req,
+//       json: true,
+//     };
+//     // console.log(options.body.body)
+//     request(options, (error, body) => {
+//       res.send(body.body.body);
+//     })
+// })
+
+
+// firestoreへの保存
+// const admin = require('firebase-admin');
+// const functions = require('firebase-functions');
+
+// admin.initializeApp(functions.config().firebase);
+// let db = admin.firestore();
+
+// exports.save = functions
+//   .region('asia-northeast1')
+//   .https.onRequest((req, res) => {
+//     let usersRef = db.collection('users');
+//       usersRef.doc('SF').set({
+//       name: 'San Francisco',
+//       country: 'USA'
+//     })
+// })
