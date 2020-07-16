49b0da397db275627c65913292316e4550dd0a39
sanoyo <sa00380304yo@gmail.com>
2020-07-16 20:16:21 +0900

    post処理修正

--- functions/index.js
+++ functions/index.js
@@ -1,42 +1,40 @@
 // Railsへのcallback
-// const functions = require('firebase-functions');
-// const express = require('express');
-// const request = require('request')
+const functions = require('firebase-functions');
+const express = require('express');
+const request = require('request')
 
-// exports.callback = functions
-//   .region('asia-northeast1')
-//   .https.onRequest((req, res) => {
-//     console.log('Receive http request')
+exports.callback = functions
+  .region('asia-northeast1')
+  .https.onRequest((req) => {
+    console.log('Receive http request')
 
-//     const options = {
-//       url: 'https://bccd9eb6fd98.ngrok.io/callback',
-//       port: '3000',
-//       method: 'POST',
-//       headers: {
-//         "content-type": "application/json"
-//       },
-//       body: req,
-//       json: true,
-//     };
-//     // console.log(options.body.body)
-//     request(options, (error, body) => {
-//       res.send(body.body.body);
-//     })
-// })
+    const options = {
+      url: 'https://6509e3d00b5d.ngrok.io/callback',
+      port: '3000',
+      method: 'POST',
+      headers: {
+        "content-type": "application/json"
+      },
+      body: req.body,
+      json: true,
+    };
+    console.log(options.body)
+    request(options)
+})
 
 
 // firestoreへの保存
-const admin = require('firebase-admin');
-const functions = require('firebase-functions');
+// const admin = require('firebase-admin');
+// const functions = require('firebase-functions');
 
-admin.initializeApp(functions.config().firebase);
-let db = admin.firestore();
+// admin.initializeApp(functions.config().firebase);
+// let db = admin.firestore();
 
-exports.authenticate = functions
-  .region('asia-northeast1')
-  .auth.user().onCreate((user) => {
-    let usersRef = db.collection('admin_users');
-      usersRef.doc(user.uid).set({
-        email: user.email
-    }) 
-});
+// exports.authenticate = functions
+//   .region('asia-northeast1')
+//   .auth.user().onCreate((user) => {
+//     let usersRef = db.collection('admin_users');
+//       usersRef.doc(user.uid).set({
+//         email: user.email
+//     }) 
+// });
