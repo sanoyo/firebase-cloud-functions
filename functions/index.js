2262507d2faa1b1a42e38522a32504ec9aaf2177
sanoyo <sa00380304yo@gmail.com>
2020-07-19 08:47:39 +0900

    FireStoreに保存できるようにした

--- functions/index.js
+++ functions/index.js
@@ -1,27 +1,42 @@
-// Railsへのcallback
 const functions = require('firebase-functions');
-const express = require('express');
+const admin = require('firebase-admin');
 const request = require('request')
+
+admin.initializeApp(functions.config().firebase);
 
 exports.callback = functions
   .region('asia-northeast1')
   .https.onRequest((req) => {
-    console.log('Receive http request')
+    const uId = req.body.events[0]['source']['userId']
+    const replyToken = req.body.events[0]['replyToken']
+    const messageType = req.body.events[0]['type']
+    const senderType = req.body.events[0]['source']['type']
+    console.log(senderType)
 
-    const options = {
-      url: 'https://6509e3d00b5d.ngrok.io/callback',
-      port: '3000',
-      method: 'POST',
-      headers: {
-        "content-type": "application/json"
-      },
-      body: req.body,
-      json: true,
-    };
-    console.log(options.body)
-    request(options)
+    // FireStoreに保存
+    // follow
+    let db = admin.firestore();
+    let usersRef = db.collection('user_posts');
+      usersRef.doc(uId).set({
+        line_id: uId,
+        message_type: messageType,
+        reply_token: replyToken,
+        sender_type: senderType
+    })
+    
+    // Railsにpost投げる
+    // const options = {c
+    //   url: 'https://6509e3d00b5d.ngrok.io/callback',
+    //   port: '3000',
+    //   method: 'POST',
+    //   headers: {
+    //     "content-type": "application/json"
+    //   },
+    //   body: req.body,
+    //   json: true,
+    // };
+    // request(options)
 })
-
 
 // firestoreへの保存
 // const admin = require('firebase-admin');
