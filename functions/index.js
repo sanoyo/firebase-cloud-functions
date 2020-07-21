c540c01f145256d9a2c507e6f6bed2a06657e63e
sanoyo <sa00380304yo@gmail.com>
2020-07-21 22:56:51 +0900

    デーモンロード優秀

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-const request = require('request')
+const request = require('request');
+const { firestore } = require('firebase-admin');
 
 // 初期化
 admin.initializeApp(functions.config().firebase);
@@ -9,75 +10,69 @@ const db = admin.firestore();
 exports.callback = functions
   .region('asia-northeast1')
   .https.onRequest((req) => {
-
     let messageType = req.body.events[0]['type']
     const uId = req.body.events[0]['source']['userId']
     const replyToken = req.body.events[0]['replyToken']
     const senderType = req.body.events[0]['source']['type']
-    const usersRef = db.collection('user_posts');
+    const usersRef = db.collection('deploy_test');
 
     // FireStoreに保存
-    if (messageType === 'follow') {
-      // ここをclass化する
-      usersRef.doc().set({
-        line_id: uId,
-        message_type: messageType,
-        reply_token: replyToken,
-        sender_type: senderType
-      })
-    } else if (messageType === 'unfollow') {
+    const saveFirestore = new Promise((resolve, reject) => {
+      if (messageType === 'follow') {
         usersRef.doc().set({
           line_id: uId,
           message_type: messageType,
+          reply_token: replyToken,
           sender_type: senderType
         })
-    } else if (messageType === 'message') {
-      messageType = req.body.events[0]['message']['type']
-      const messgae = req.body.events[0]['message']['text']
-
-      usersRef.doc().set({
-        line_id: uId,
-        message: { text: messgae },
-        message_type: messageType,
-        reply_token: replyToken,
-        sender_type: senderType
-      })
-    } else if (messageType === 'postback') {
-      const data = req.body.events[0]['postback']['data']
-
-      usersRef.doc().set({
-        line_id: uId,
-        message: { 
-          data: data,
-          params: {
-            datetime: '11111'
-          }
-        },
-        message_type: messageType,
-        reply_token: replyToken,
-        sender_type: senderType
-      })
-    }
-
-    // Railsにpost投げる
-    const options = {
-      url: 'http://localhost:3000/callback',
-      method: 'POST',
-      headers: {
-        "content-type": "application/json"
-      },
-      body: req.body,
-      json: true,
-    };
-    request(options)
-})
-
-// firestoreへの保存
-exports.authenticate = functions
-  .region('asia-northeast1')
-  .auth.user().onCreate((user) => {
-    let usersRef = db.collection('admin_users');
-      usersRef.doc(user.uid).set({
-        email: user.email
+      } else if (messageType === 'unfollow') {
+          usersRef.doc().set({
+            line_id: uId,
+            message_type: messageType,
+            sender_type: senderType
+          })
+      } else if (messageType === 'message') {
+        messageType = req.body.events[0]['message']['type']
+        const messgae = req.body.events[0]['message']['text']
+  
+        usersRef.doc().set({
+          line_id: uId,
+          message: { text: messgae },
+          message_type: messageType,
+          reply_token: replyToken,
+          sender_type: senderType
+        })
+      } else if (messageType === 'postback') {
+        const data = req.body.events[0]['postback']['data']
+  
+        usersRef.doc().set({
+          line_id: uId,
+          message: { 
+            data: data,
+            params: {
+              datetime: '11111'
+            }
+          },
+          message_type: messageType,
+          reply_token: replyToken,
+          sender_type: senderType
+        })
+      }
+      resolve('Success!');
     })
-});
+    
+    saveFirestore.then(() => {
+      const options = {
+        url: 'http://localhost:3000/callback',
+        method: 'POST',
+        headers: {
+          "content-type": "application/json"
+        },
+        body: req.body,
+        json: true,
+      };
+      request(options)
+    }).catch((error) => { 
+      console.log(error);
+    });;
+})
